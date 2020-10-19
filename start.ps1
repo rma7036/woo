fd202f2357efdc8c0b85ce37c388f386306929e7
Robert Allen <Robert.Allen@vwonmicrosoft.com>
2020-10-19 23:37:37 +0000

    saving

--- start.ps1
+++ start.ps1
@@ -10,5 +10,5 @@ docker rm $node2
 
 $CONFIG_URI="https://raw.githubusercontent.com/rma7036/woo/main/cluster-config.xml"
 $JVM_OPTS="-Xms1g -Xmx1g -server -XX:+AggressiveOpts -XX:MaxPermSize=256m"
-docker run --ip 172.17.0.21 -d --name $node1 -e "JVM_OPTS=$JVM_OPTS" -e "CONFIG_URI=$CONFIG_URI" $image
-docker run --ip 172.17.0.22 -d --name $node2 -e "JVM_OPTS=$JVM_OPTS" -e "CONFIG_URI=$CONFIG_URI" $image
+docker run --ip 172.17.10.1 -d --name $node1 -e "JVM_OPTS=$JVM_OPTS" -e "CONFIG_URI=$CONFIG_URI" $image
+docker run --ip 172.17.10.2 -d --name $node2 -e "JVM_OPTS=$JVM_OPTS" -e "CONFIG_URI=$CONFIG_URI" $image
